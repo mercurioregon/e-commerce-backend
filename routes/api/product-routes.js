dab30249574679c25ed233f264f4ce0d0a970b88
mercurioregon <jamesm620@gmail.com>
2024-05-15 18:54:27 -0700

    sequelize models complete. Routes to do

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,10 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+    const data= await Product.findAll({raw:true})
+    console.log(data)
+    res.json(data)
 });
 
 // get one product
